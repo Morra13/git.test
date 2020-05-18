f18991f4856740a096f4caad43ab5830629a8685
Mikhail Shabanov <moraisha1317@yandex.ru>
2020-05-18 14:54:29 +0400

    Регистрация

--- registration/reg.php
+++ registration/reg.php
@@ -1,73 +1,87 @@
 <?php
 $FileName = 'D:\OSPanel\domains\git.test\registration\user.json';
 $json1 = file_get_contents($FileName);
-$jsonD = json_decode($json1, true);
+$arrOldUsers = json_decode($json1, true);
 
-    $data = $_POST;
-    if(isset($data['reg'])){
+$data = $_POST;
+if(isset($data['reg'])){
 
-        $errors = [];
-        if(trim($data['login'] == $jsonD['login'])){
-            $errors[] = 'Такой логин уже есть!';
-        }
+    $errors = [];
+    if(trim($data['login'] === $arrOldUsers['login'])){
+        $errors[] = 'Такой логин уже есть!';
+    }
 
-        if(trim($data['email'] == $jsonD['email'])){
-            $errors[] = 'Такой email уже есть!';
-        }
+    if(trim($data['email'] === $arrOldUsers['email'])){
+        $errors[] = 'Такой email уже есть!';
+    }
+    if(trim($data['login'] == '')){
+        $errors[] = 'Введите логин!';
+    }
+    if(trim($data['email'] == '')){
+        $errors[] = 'Введите email!';
+    }
+    if($data['password'] == ''){
+        $errors[] = 'Введите password!';
+    }
 
-        if(trim($data['login'] == '')){
-            $errors[] = 'Введите логин!';
-        }
-        if(trim($data['email'] == '')){
-            $errors[] = 'Введите email!';
-        }
-        if($data['password'] == ''){
-            $errors[] = 'Введите password!';
-        }
+    if($data['password_2'] != $data['password']){
+        $errors[] = 'Повторный пароль не верный';
+    }
 
-        if($data['password_2'] != $data['password']){
-            $errors[] = 'Повторный пароль не верный';
-        }
+if(empty($errors) && isset($arrOldUsers)){
+    $arrUser = [
+        'login' => $data['login'],
+        'email' => $data['email'],
+        'password' => md5($data['password'])
+    ];
 
+        $arrNewUser = $arrUser;
 
-        if(empty($errors)){
-            $user = [
+        $arrAllUsers = [$arrOldUsers, $arrNewUser];
+        $json2 = json_encode($arrAllUsers, JSON_UNESCAPED_UNICODE);
+
+        file_put_contents($FileName, $json2);
+
+}
+
+    elseif(empty($errors)){
+        $firstUser[] = [
             'login' => $data['login'],
             'email' => $data['email'],
             'password' => md5($data['password'])
         ];
-            $json = json_encode($user, JSON_UNESCAPED_UNICODE);
+        $json = json_encode($firstUser, JSON_UNESCAPED_UNICODE);
 
-            if (file_exists($FileName)) {
-                file_put_contents($FileName, $json . '
+        if (file_exists($FileName)) {
+            file_put_contents($FileName, $json . '
         '  , FILE_APPEND);
-            }
-
-            echo '<div style="color: green"> Вы зарегистрировались! </div><hr/>';
-        }else{
-            echo '<div style="color: red">' . array_shift($errors) . '</div><hr/>';
         }
 
+        echo '<div style="color: green"> Вы зарегистрировались! </div><hr/>';
+    }else{
+        echo '<div style="color: red">' . array_shift($errors) . '</div><hr/>';
     }
+
+}
 
 ?>
 
 <form action="/registration/reg.php" method="post">
     <p>
-        <p>Логин</p>
-        <input type="text" name="login" value="<?php //echo @$data['login']?>">
+    <p>Логин</p>
+    <input type="text" name="login" value="<?php //echo @$data['login']?>">
     </p>
     <p>
-        <p>Емайл</p>
-        <input type="email" name="email" value="<?php //echo @$data['email']?>">
+    <p>Емайл</p>
+    <input type="email" name="email" value="<?php //echo @$data['email']?>">
     </p>
     <p>
-        <p>Пароль</p>
-        <input type="password" name="password" value="<?php //echo @$data['password']?>">
+    <p>Пароль</p>
+    <input type="password" name="password" value="<?php //echo @$data['password']?>">
     </p>
     <p>
-        <p>Повторите пароль</p>
-        <input type="password" name="password_2" value="<?php //echo @$data['password_2']?>">
+    <p>Повторите пароль</p>
+    <input type="password" name="password_2" value="<?php //echo @$data['password_2']?>">
     </p>
 
     <p>
